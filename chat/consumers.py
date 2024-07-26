83001532baed88844511378787b4ed737ed5cb69
linustoernqvist <linus.tornqvist@triggerbee.com>
2024-07-26 16:48:31 -0400

    a

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,23 +1,13 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
-from asgiref.sync import async_to_sync
 import google.generativeai as genai
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        # self.room_group_name = 'test'
-
-        # async_to_sync(self.channel_layer.group_add)(
-        #     self.room_group_name,
-        #     self.channel_name
-        # )
         self.init_chat()
-
         self.accept()
-
         self.send(text_data=json.dumps({'message': 'Connected'}))
    
-
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
@@ -25,14 +15,6 @@ class ChatConsumer(WebsocketConsumer):
         response = self.chat.send_message(message)
         print(response.text)
         self.send(text_data=json.dumps({'response': response.text}))
-
-        # async_to_sync(self.channel_layer.group_send)(
-        #     self.room_group_name,
-        #     {
-        #         'type':'chat_message',
-        #         'message':message
-        #     }
-        # )
 
     def init_chat(self):
         genai.configure(api_key='AIzaSyAeZ6pEPeJ_MIR8N0QQCZq-vLbyuVTJMy4')
